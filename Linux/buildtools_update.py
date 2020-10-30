7470f91f9a92a3cc3e39f79b62ab70332e865389
Szabolcs Kavalecz <61993364+szabi1035@users.noreply.github.com>
2020-10-30 22:46:32 +0100

    Trying to get files into working condiiton

--- Linux/buildtools_update.py
+++ Linux/buildtools_update.py
@@ -34,7 +34,6 @@ import sys
 ###############################
 slash = ""
 currentDirectory = ""
-serverDirectory = ""
 systemType = ""
 ##############################
 #       Main functions       #
@@ -60,28 +59,15 @@ def get_cwd():
     currentDirectory = os.getcwd() #gets the Current Working Directory
     print("This script is located in " + currentDirectory + "\nIt will be used as the install directory!")
 
-def pathconf():
-    global currentDirectory
-    global serverDirectory
-
-    answer = input("Would you like to change that? (Y/N) ")
-    if answer == "Y":
-        serverDirectory = input("Input path to the server directory: ")
-    elif answer == "N":
-        serverDirectory = currentDirectory
-    else:
-        print("Invalid input!")
-        pathconf()
-
 def slashcheck():
-    global serverDirectory
-    if serverDirectory[-1] != slash:
-        serverDirectory = serverDirectory + slash
+    global currentDirectory
+    if currentDirectory[-1] != slash:
+        currentDirectory = currentDirectory + slash
 
 def download_file():
     url = "https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar"
 
-    print("Downloading BuildTools.jar to " + serverDirectory)
+    print("Downloading BuildTools.jar to " + currentDirectory)
     r = requests.get(url) # create the HTTP response object
     rstr = str(r) #makes it into a string (debug purposes,and feedback for the user)
 
@@ -92,7 +78,7 @@ def download_file():
         print("HTTP Response code abnormal! Aborting!!!\nIF THIS ISSUE PERSISTS github.com/szabi1035/Minecraft-spigot-updater/issues")
         sys.exit(0)
 
-    with open(serverDirectory + "BuildTools.jar",'wb') as f: 
+    with open(currentDirectory + "BuildTools.jar",'wb') as f: 
         # Saving received content as a jar file in 
         # binary format 
         # write the contents of the response (r.content) 
@@ -107,12 +93,12 @@ def rename_serverfile():
     for filename in filenames:
         if "spigot-1." in filename:
             print("Server file found: " + filename + "\nRenaming " + filename + " to server.jar")
-            os.rename(serverDirectory + slash + filename, serverDirectory + slash + "server.jar")
+            os.rename(currentDirectory + slash + filename, currentDirectory + slash + "server.jar")
 
 def run_prompt():
     answer = input("Would you like to run the server now? (Y/N) ")
     if answer == "Y":
-        os.system('runserver.py')
+        os.system('python3 runserver.py')
     elif answer == "N":
         sys.exit(0)
     else:
@@ -123,7 +109,6 @@ def run_prompt():
 ##########################
 systemtypef()
 get_cwd()
-pathconf()
 slashcheck()
 download_file()
 run_buildtoolsjar()
